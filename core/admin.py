cbeb529913afc658a251a1fdeb51ff9dae05c0ac
CIbarra310 <chasen.ibarra@mac.com>
2024-05-16 16:34:17 -0700

    Model Registration
    models registered in admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Production, Vendor, Location, Department, JobTitle
 
 # Register your models here.
+admin.site.register(Production)
+admin.site.register(Vendor)
+admin.site.register(Location)
+admin.site.register(Department)
+admin.site.register(JobTitle)
