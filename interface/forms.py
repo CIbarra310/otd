a26e418860140c5e88a8e6d38bf58ef01d0cb1e6
CIbarra310 <chasen.ibarra@mac.com>
2024-05-20 09:57:54 -0700

    Multiple Updates from original app

---
+++ interface/forms.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.forms.widgets import PasswordInput, TextInput
+from django.utils import timezone
+from core.models import Production, Department, JobTitle
+
+# - Register/Create a user
+class CreateUserForm(UserCreationForm):    
+    email = forms.EmailField()
+    user_name = forms.CharField(max_length=150)
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    phone_number = forms.CharField(max_length=20)
+    production_title = forms.ModelChoiceField(
+        queryset=Production.objects.filter(is_active=True).order_by('production_title'),
+        empty_label="Select Production Title"
+    )
+    department = forms.ModelChoiceField(
+        queryset=Department.objects.order_by('department_title'),
+        empty_label="Select Department"
+    )
+    job_title = forms.ModelChoiceField(
+        queryset=JobTitle.objects.all(),
+        empty_label="Select Job Title"
+    )
+    # job_title = forms.CharField(max_length=100)
+
+    class Meta:
+        model = User
+        fields = ['username','email', 'password1', 'password2', 'first_name', 'last_name', 'job_title', 'department', 'production_title']
+    
+
+
+# - Login a user
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
--- interface/views.py
+++ interface/views.py
@@ -1,5 +1,52 @@
-from django.shortcuts import render
+from .forms import LoginForm, CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import auth
+from django.db.models.functions import Lower
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+import json
 
 # - Home Page
 def home(request):
     return render(request, 'interface/index.html')
+
+# - Register a new user
+def register(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created successfully!")
+            return redirect("login")
+        
+    context = {'form':form}  
+    return render(request, 'app/register.html', context = context) 
+
+# - Login an existing user
+def login(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect("dashboard")
+    
+    context = {'form':form}
+    return render(request, 'app/login.html', context=context)
+
+# - Logout a user
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "You have been logged out")
+    return redirect("login")
