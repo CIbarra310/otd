d729d0c21ecf0f221dba2ff9e7696f3ad721aa34
Chasen Ibarra <chasen.ibarra@mac.com>
2024-07-17 10:02:24 -0700

    Update

--- transportation/models.py
+++ transportation/models.py
@@ -31,10 +31,13 @@ class Vehicle(models.Model):
         return self.vehicle_type + " " + self.vendor_unit_number
 
 class RunRequest(models.Model):
+    # - Assign production to the run
     production_title = models.CharField(max_length=150, null=True)
-    #Run Create Date
+
+    # - Run Create Date
     create_date = models.DateField(auto_now_add=True)
-    #Requester Information
+    
+    # - Requester Information
     requester_name = models.CharField(max_length=100)
     requester_phone = models.CharField(max_length=15)
     requester_email = models.CharField(max_length=255)
@@ -42,8 +45,9 @@ class RunRequest(models.Model):
     run_date = models.DateField()
     ready_time = models.TimeField(null=True, blank=True)
     need_by_this_time = models.TimeField(null=True, blank=True)
+    run_type = models.CharField(null=True, max_length=50)
 
-    #Pick Up Information
+    # - Stop 1 Information
     pickup_name = models.CharField(max_length=100)
     pickup_phone = models.CharField(max_length=15, null=True, blank=True)
     pickup_address_1 = models.CharField(max_length=100)
@@ -52,22 +56,62 @@ class RunRequest(models.Model):
     pickup_state = models.CharField(max_length=2)
     pickup_zip = models.CharField(max_length=10)
 
-    #Drop Off Information
-    dropoff_name = models.CharField(max_length=100)
+    # - Stop 2 Information
+    dropoff_name = models.CharField(max_length=100, blank=True)
     dropoff_phone = models.CharField(max_length=15, blank=True)
-    dropoff_address_1 = models.CharField(max_length=100)
+    dropoff_address_1 = models.CharField(max_length=100, blank=True)
     dropoff_address_2 = models.CharField(max_length=100, null=True, blank=True)
-    dropoff_city = models.CharField(max_length=100)
-    dropoff_state = models.CharField(max_length=2)
-    dropoff_zip = models.CharField(max_length=10)
+    dropoff_city = models.CharField(max_length=100, blank=True)
+    dropoff_state = models.CharField(max_length=2, blank=True)
+    dropoff_zip = models.CharField(max_length=10, blank=True)
 
-    #Run Details
+    # - Stop 3 Information
+    stop_3_name = models.CharField(max_length=100, blank=True)
+    stop_3_phone = models.CharField(max_length=15, blank=True)
+    stop_3_address_1 = models.CharField(max_length=100, blank=True)
+    stop_3_address_2 = models.CharField(max_length=100, null=True, blank=True)
+    stop_3_city = models.CharField(max_length=100, blank=True)
+    stop_3_state = models.CharField(max_length=2, blank=True)
+    stop_3_zip = models.CharField(max_length=10, blank=True)
+
+    # - Stop 4 Information
+    stop_4_name = models.CharField(max_length=100, blank=True)
+    stop_4_phone = models.CharField(max_length=15, blank=True)
+    stop_4_address_1 = models.CharField(max_length=100, blank=True)
+    stop_4_address_2 = models.CharField(max_length=100, null=True, blank=True)
+    stop_4_city = models.CharField(max_length=100, blank=True)
+    stop_4_state = models.CharField(max_length=2, blank=True)
+    stop_4_zip = models.CharField(max_length=10, blank=True)
+
+    # - Stop 5 Information
+    stop_5_name = models.CharField(max_length=100, blank=True)
+    stop_5_phone = models.CharField(max_length=15, blank=True)
+    stop_5_address_1 = models.CharField(max_length=100, blank=True)
+    stop_5_address_2 = models.CharField(max_length=100, null=True, blank=True)
+    stop_5_city = models.CharField(max_length=100, blank=True)
+    stop_5_state = models.CharField(max_length=2, blank=True)
+    stop_5_zip = models.CharField(max_length=10, blank=True)
+
+    # - Stop 6 Information
+    stop_6_name = models.CharField(max_length=100, blank=True)
+    stop_6_phone = models.CharField(max_length=15, blank=True)
+    stop_6_address_1 = models.CharField(max_length=100, blank=True)
+    stop_6_address_2 = models.CharField(max_length=100, null=True, blank=True)
+    stop_6_city = models.CharField(max_length=100, blank=True)
+    stop_6_state = models.CharField(max_length=2, blank=True)
+    stop_6_zip = models.CharField(max_length=10, blank=True)
+
+    # - Run Details
     purchase_order = models.CharField(max_length=20, null=True, blank=True)
     vendor_invoice  = models.CharField(max_length=20, null=True, blank=True)
     run_status = models.CharField(max_length=20, null=True, blank=True)
     truck_size = models.CharField(max_length=50, null=True, blank=True)
     run_details = models.TextField(max_length=500)
     assigned_driver = models.CharField(max_length=100, null=True, blank=True)
+
+    # - Transportation Notes
+    transportation_notes = models.TextField(max_length=2000, null=True, blank=True)
+    driver_notes = models.TextField(max_length=2000, null=True, blank=True)
 
     def __str__(self):
         return "Run #" + str(self.id) + " - " + self.requester_department
