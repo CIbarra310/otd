5659f89250f19e492956f3b62a2724b9e5ea4302
Chasen Ibarra <chasen.ibarra@mac.com>
2024-10-21 20:31:09 -0700

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -9,7 +9,7 @@ class UserAdminConfig(UserAdmin):
     search_fields = ('productions', 'department', 'job_title')
     list_filter = ('productions', 'department', 'job_title')
     ordering = ('-start_date',)
-    list_display = ('email', 'username', 'first_name', 'last_name','phone_number', 'password1', 'password2', 'production_title', 'department', 'job_title', 'is_staff', 'is_active')
+    list_display = ('email', 'username', 'first_name', 'last_name','phone_number', 'password', 'production_title', 'department', 'job_title', 'is_staff', 'is_active')
     fieldsets = (
         ('User Information', {'fields': ('email', 'username', 'first_name', 'last_name', 'phone_number', 'password1', 'password2')}),
         ('Production Information', {'fields': ('productions', 'department', 'job_title',)}),
@@ -18,7 +18,7 @@ class UserAdminConfig(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'username', 'first_name', 'last_name','password1', 'password2', 'phone_number', 'production_title', 'department', 'job_title', 'is_staff', 'is_active')}
+            'fields': ('email', 'username', 'first_name', 'last_name','password', 'phone_number', 'production_title', 'department', 'job_title', 'is_staff', 'is_active')}
          ),
     )
 
