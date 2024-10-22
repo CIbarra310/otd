02faa1bae360b4c6152f57c93201897d4dc72246
Chasen Ibarra <chasen.ibarra@mac.com>
2024-10-21 18:45:47 -0700

    Update forms.py

--- interface/forms.py
+++ interface/forms.py
@@ -12,7 +12,7 @@ from core.models import Production
 # - Register/Create a user
 class CreateUserForm(UserCreationForm):    
     email = forms.EmailField()
-    user_name = forms.CharField(max_length=150)
+    username = forms.CharField(max_length=150)  # Changed from user_name to username
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
     phone_number = forms.CharField(max_length=20)
@@ -28,12 +28,22 @@ class CreateUserForm(UserCreationForm):
         queryset=JobTitle.objects.order_by('job_title'),
         empty_label="Select Job Title"
     )
-    # job_title = forms.CharField(max_length=100)
-
     class Meta:
         model = NewUser
-        fields = ['user_name','email', 'password1', 'password2', 'first_name', 'last_name', 'job_title', 'department', 'production_title']
+        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'phone_number', 'production_title', 'department', 'job_title']
 
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.phone_number = self.cleaned_data['phone_number']
+        user.production_title = self.cleaned_data['production_title']
+        user.department = self.cleaned_data['department']
+        user.job_title = self.cleaned_data['job_title']
+        if commit:
+            user.save()
+        return user
 
 # - Login a user
 class LoginForm(AuthenticationForm):
