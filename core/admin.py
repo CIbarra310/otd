05def38b784cdc591d09074800e4607aa8ef1e16
Chasen Ibarra <chasen.ibarra@mac.com>
2024-10-21 20:15:37 -0700

    Login updates

--- core/admin.py
+++ core/admin.py
@@ -9,9 +9,9 @@ class UserAdminConfig(UserAdmin):
     search_fields = ('productions', 'department', 'job_title')
     list_filter = ('productions', 'department', 'job_title')
     ordering = ('-start_date',)
-    list_display = ('email', 'username', 'first_name', 'last_name','phone_number', 'production_title', 'department', 'job_title', 'is_staff', 'is_active')
+    list_display = ('email', 'username', 'first_name', 'last_name','phone_number', 'password' 'production_title', 'department', 'job_title', 'is_staff', 'is_active')
     fieldsets = (
-        ('User Information', {'fields': ('email', 'username', 'first_name', 'last_name', 'phone_number')}),
+        ('User Information', {'fields': ('email', 'username', 'first_name', 'last_name', 'phone_number', 'password')}),
         ('Production Information', {'fields': ('productions', 'department', 'job_title',)}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'groups')}),
     )
